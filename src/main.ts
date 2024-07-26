d4ca907fd1232c4577facac738c8e855e0a5b6e7
ilyas abduttawab <ilyas@peasy.ai>
2024-07-26 14:02:59 +0700

    add root url and disable swagger in other than local

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { RequestMethod } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+
+  app.setGlobalPrefix('api', {
+    exclude: [{ path: '/', method: RequestMethod.GET }],
+  });
+
   app.enableCors();
+  console.log(process.env.NODE_ENV)
+  if (process.env.APP_ENV == 'local') {
+    const config = new DocumentBuilder()
+      .addBearerAuth()
+      .setTitle('Wallet Transaction API')
+      .setDescription('The Wallet Transaction API description')
+      .setVersion('0.1')
+      .build();
 
-  const config = new DocumentBuilder()
-    .addBearerAuth()
-    .setTitle('Wallet Transaction API')
-    .setDescription('The Wallet Transaction API description')
-    .setVersion('0.1')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swagger', app, document, {
+      jsonDocumentUrl: 'swagger/json',
+    });
+  }
 
   await app.listen(3000);
 }
