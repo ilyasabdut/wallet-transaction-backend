11162899f656e4b55ae966ee917fa4df2a80b10e
ilyas abduttawab <ilyas@peasy.ai>
2024-07-27 00:20:37 +0700

    add sentry for logging

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    throw new Error('Ops, theres a problem!');
+    // throw new Error('Ops, theres a problem!');
     return 'Hello World!, Welcome to Wallet Tranction API';
   }
 }
